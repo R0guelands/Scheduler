f671a92688161ef3974314341198429811773bf7
R0guelands <marcelogranzotto@hotmail.com.br>
2023-03-02 03:27:54 +0000

    front end pronto

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 def index():
+    print("salve da home")
     return render_template('home.html')
+
+
+@app.route('/history')
+def history():
+    print("salve da history")
+    return render_template('history.html')
