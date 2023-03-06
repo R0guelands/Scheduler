683f1a2ebb0406e354eaa889a7ad716c32704fd5
R0guelands <marcelogranzotto@hotmail.com.br>
2023-03-06 03:08:00 +0000

    bug fix weekly

--- time_trigger_handler.py
+++ time_trigger_handler.py
@@ -31,24 +31,20 @@ task_table = pd.DataFrame(columns=["Name", "Time"])
 
 
 def get_next_sixth_hours(num_hours=24):
-    # Get current date and time in Brazil timezone
-    tz_brazil = pytz.timezone('America/Sao_Paulo')
+    tz_brazil = pytz.timezone("America/Sao_Paulo")
     now = datetime.datetime.now(tz=tz_brazil)
-
-    # Calculate the start of the current day in Brazil timezone
-    start_of_day = tz_brazil.localize(datetime.datetime(now.year, now.month, now.day, 0, 0, 0))
-
-    # Calculate the number of hours that have passed since the start of the day
+    start_of_day = tz_brazil.localize(
+        datetime.datetime(now.year, now.month, now.day, 0, 0, 0)
+    )
     hours_passed = (now - start_of_day).seconds // 3600
-
-    # Calculate the next 6th hour for the next num_hours
     next_sixth_hours = []
     for i in range(num_hours // 6):
         next_sixth_hour = ((hours_passed // 6) + 1 + i) * 6
         next_sixth_hour_dt = start_of_day + datetime.timedelta(hours=next_sixth_hour)
         next_sixth_hours.append(next_sixth_hour_dt)
 
-    return pd.DataFrame({'Datetime': next_sixth_hours})
+    return pd.DataFrame({"Datetime": next_sixth_hours})
+
 
 def run():
     tasks = list(time_trigger.find({}))
@@ -58,56 +54,63 @@ def run():
     for task in tasks:
         if "daily" in task["config"]:
             if len(task["config"]["daily"]) == 0:
-                task["config"]["daily"] = ["00:00"]
+                task["config"]["daily"] = ["00:01"]
             for hour in task["config"]["daily"]:
+                if hour == "00:00":
+                    hour = "00:01"
                 hour = datetime.datetime.strptime(hour, "%H:%M")
-                now = datetime.datetime.now(tz=pytz.timezone('America/Sao_Paulo'))
-                hour_ = now.replace(hour=hour.hour, minute=hour.minute, second=0, microsecond=0)
+                now = datetime.datetime.now(tz=pytz.timezone("America/Sao_Paulo"))
+                hour_ = now.replace(
+                    hour=hour.hour, minute=hour.minute, second=0, microsecond=0
+                )
                 if hour_ >= now and hour_ <= max_time_table.loc[0, "Datetime"]:
                     task_table.loc[len(task_table)] = [task["name"], hour_]
 
         if "weekly" in task["config"]:
             if len(task["config"]["weekly"]) == 0:
-                task["config"]["weekly"] = {
-                    "0": ["00:00"]
-                }
+                task["config"]["weekly"] = {"0": ["00:01"]}
             for day in task["config"]["weekly"]:
                 if len(task["config"]["weekly"][day]) == 0:
-                    task["config"]["weekly"][day] = ["00:00"]
+                    task["config"]["weekly"][day] = ["00:01"]
                 for hour in task["config"]["weekly"][day]:
-                    now = datetime.datetime.now(tz=pytz.timezone('America/Sao_Paulo'))
+                    if hour == "00:00":
+                        hour = "00:01"
+                    now = datetime.datetime.now(tz=pytz.timezone("America/Sao_Paulo"))
                     if int(day) == now.weekday():
                         hour = datetime.datetime.strptime(hour, "%H:%M")
-                        hour_ = now.replace(hour=hour.hour, minute=hour.minute, second=0, microsecond=0)
+                        hour_ = now.replace(
+                            hour=hour.hour, minute=hour.minute, second=0, microsecond=0
+                        )
                         if hour_ >= now and hour_ <= max_time_table.loc[0, "Datetime"]:
                             task_table.loc[len(task_table)] = [task["name"], hour_]
-                        
+
         if "monthly" in task["config"]:
             if len(task["config"]["monthly"]) == 0:
-                task["config"]["monthly"] = {
-                    "0": ["00:00"]
-                }
+                task["config"]["monthly"] = {"1": ["00:01"]}
             for day in task["config"]["monthly"]:
                 if len(task["config"]["monthly"][day]) == 0:
-                    task["config"]["monthly"][day] = ["00:00"]
+                    task["config"]["monthly"][day] = ["00:01"]
                 for hour in task["config"]["monthly"][day]:
-                    now = datetime.datetime.now(tz=pytz.timezone('America/Sao_Paulo'))
+                    if hour == "00:00":
+                        hour = "00:01"
+                    now = datetime.datetime.now(tz=pytz.timezone("America/Sao_Paulo"))
                     if int(day) == now.day:
                         hour = datetime.datetime.strptime(hour, "%H:%M")
-                        hour_ = now.replace(hour=hour.hour, minute=hour.minute, second=0, microsecond=0)
+                        hour_ = now.replace(
+                            hour=hour.hour, minute=hour.minute, second=0, microsecond=0
+                        )
                         if hour_ >= now and hour_ <= max_time_table.loc[0, "Datetime"]:
                             task_table.loc[len(task_table)] = [task["name"], hour_]
-            
-    groups = task_table.groupby('Time').filter(lambda x: len(x) > 1).groupby('Time')
+
+    groups = task_table.groupby("Time").filter(lambda x: len(x) > 1).groupby("Time")
     task_table_list = [group.reset_index(drop=True) for _, group in groups]
-    unique_times = task_table.groupby('Time').filter(lambda x: len(x) == 1)
-    task_table_list.extend([unique_times.loc[[i]].reset_index(drop=True) for i in unique_times.index])
-    task_table_list.sort(key=lambda x: x['Time'].iloc[0])
+    unique_times = task_table.groupby("Time").filter(lambda x: len(x) == 1)
+    task_table_list.extend(
+        [unique_times.loc[[i]].reset_index(drop=True) for i in unique_times.index]
+    )
+    task_table_list.sort(key=lambda x: x["Time"].iloc[0])
 
     for task_table in task_table_list:
         print(task_table)
 
-
-
 run()
-        
